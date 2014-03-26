c1e88b3d38032a242e054aa5b72b65a31d4480f5
BeaumontTaz <bryan.b.danley@gmail.com>
2014-03-26 18:39:15 -0400

    Git doesn't commit changes automatically? We have to manually add any files we edit?

--- scripts/cities.js
+++ scripts/cities.js
@@ -1,6 +1,7 @@
 function clicked()
 {
 	alert("I'VE BEEN CLICKED! CALL AN AMBULANCE!")
+	$("#output").html("HE'S BEEN CLICKED!");
 }
 alert(FIPS_StateName[0].join());
 alert(FIPS_CountyName[100].join());

2e93975b657f525815f68deb8306aad9e9e24766
BeaumontTaz <bryan.b.danley@gmail.com>
2014-03-27 23:21:16 -0400

    More work on visible and search stuff

--- scripts/cities.js
+++ scripts/cities.js
@@ -5,73 +5,66 @@ function search_f()
 	var t = $("input[type='radio']:checked").val();
 	var matches = new Array();
 	m = 0;
-	if (t == 0) // By county
+	if(s.length<3)
 	{
-		if(s.length<3)
+		// Case when search criteria is short
+		alert("LONGER SEARCH PL0XRS");
+	}
+	else
+	{
+		// Matching by 3 letter segments
+		spell_cases = s.length - 2;
+		for (s_case = 0; s_case < spell_cases; s_case++)
 		{
-			// Case when search criteria is short
-			alert("LONGER SEARCH PL0XRS");
+			for (i = 0; i < FIPS_CountyName.length; i++)
+			{
+				s_case_end = s_case + 3;
+				s_n = FIPS_CountyName[i][1].toLowerCase();
+				s_m = s.slice(s_case, s_case_end);
+				if (s_n.match(s_m))
+				{
+					matches[m] = i;
+					m++
+				}
+			}
+		}	
+		matches.sort(sortNumber);
+		var fin_m = new Array();
+		fin_m[0] = [0,0];
+		
+		for (i = 0; i < FIPS_CountyName.length; i++)
+		{
+			temp = 0;
+			for (r = 0; r < matches.length; r ++)
+			{
+				if (i == matches[r])
+				{
+					temp ++;
+				}
+			}
+			if (temp != 0)
+			{
+				fin_m.push([temp,i]);
+			}
+		}
+		(fin_m.sort()).reverse();
+		if (fin_m[0][0] == 0)
+		{
+			// No search results
+			$("#output").html("No search results found. Please refine your search.");
 		}
 		else
 		{
-			// Matching by 3 letter segments
-			spell_cases = s.length - 2;
-			for (s_case = 0; s_case < spell_cases; s_case++)
+			// at elast 1 results
+			$("#output").html("The following counties match your search:<br/>");
+			i = 0;
+			// WHAT IF fin_m[i][0] == length of word - 2?
+			while (fin_m[i][0] > fin_m[0][0]/2.0)
 			{
-				for (i = 0; i < FIPS_CountyName.length; i++)
-				{
-					s_case_end = s_case + 3;
-					s_n = FIPS_CountyName[i][1].toLowerCase();
-					s_m = s.slice(s_case, s_case_end);
-					if (s_n.match(s_m))
-					{
-						matches[m] = i;
-						m++
-					}
-				}
-			}	
-			matches.sort(sortNumber);
-			var fin_m = new Array();
-			fin_m[0] = [0,0];
-			
-			for (i = 0; i < FIPS_CountyName.length; i++)
-			{
-				temp = 0;
-				for (r = 0; r < matches.length; r ++)
-				{
-					if (i == matches[r])
-					{
-						temp ++;
-					}
-				}
-				if (temp != 0)
-				{
-					fin_m.push([temp,i]);
-				}
-			}
-			(fin_m.sort()).reverse();
-			if (fin_m[0][0] == 0)
-			{
-				// No search results
-				$("#output").html("No search results found. Please refine your search.");
-			}
-			else
-			{
-				// at elast 1 results
-				$("#output").html("The following counties match your search:<br/>");
-				i = 0;
-				// WHAT IF fin_m[i][0] == length of word - 2?
-				while (fin_m[i][0] > fin_m[0][0]/2.0)
-				{
-					$("#output").append(FIPS_CountyName[fin_m[i][1]] + ", " + fin_m[i][0] + "<br>");
-					i++
-				}
+				$("#output").append(FIPS_CountyName[fin_m[i][1]][1] + ", " + FIPS_CountyName[fin_m[i][1]][2] + "<br>");
+				i++
 			}
 		}
-	}
-	else // By State
-	{
-
 	}
 }
 

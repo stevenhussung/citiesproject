b0b397475c6d26a3a5d7608fafd9d39f7377d092
BeaumontTaz <bryan.b.danley@gmail.com>
2014-03-29 00:35:07 -0400

    Formatting list from search

--- scripts/cities.js
+++ scripts/cities.js
@@ -1,6 +1,31 @@
 function search_f()
 {
 	var s = ($("#search").val()).toLowerCase();
+	a = s.length;
+	if ((s.slice(a-7,a)).localeCompare(" county") == 0)
+	{
+		s = s.slice(0,a-7)
+	}
+	else if ((s.slice(a-17,a)).localeCompare(" city and borough") == 0)
+	{
+		s = s.slice(0,a-17);
+	}
+	else if ((s.slice(a-8,a)).localeCompare(" borough") == 0)
+	{
+		s = s.slice(0,a-8);
+	}
+	else if ((s.slice(a-7,a)).localeCompare(" parish") == 0)
+	{
+		s = s.slice(0,a-7);
+	}
+	else if ((s.slice(a-12,a)).localeCompare(" census area") == 0)
+	{
+		s = s.slice(0,a-12);
+	}
+	else if ((s.slice(a-13,a)).localeCompare(" municipality") == 0)
+	{
+		s = s.slice(0,a-13);
+	}
 	var t = $("input[type='radio']:checked").val();
 	var matches = new Array();
 	m = 0;
@@ -58,12 +83,12 @@ function search_f()
 			// at elast 1 results
 			i = 0;
 			// WHAT IF fin_m[i][0] == length of word - 2?
-			while (fin_m[i][0] > fin_m[0][0]/2.0 && i < 42)
+			while (fin_m[i][0] > fin_m[0][0]/2.0 && i < 24)
 			{
 				j = i % 3 + 1;
 				n = "list" + j.toString();
 				b = FIPS_CountyName[fin_m[i][1]][1];
-				$("#output table #" + n).append("<div id='state_abbr'>"+FIPS_CountyName[fin_m[i][1]][2]+"</div>"+b+"<br>");
+				$("#output table #" + n).append("<div class='list_elem'><div class='state_abbr'>"+FIPS_CountyName[fin_m[i][1]][2]+"</div>"+b+"</div>");
 				i++
 			}
 		}

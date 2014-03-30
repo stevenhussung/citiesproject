6b218b44ae7c10292ed711a9af021df1f97e74a4
BeaumontTaz <bryan.b.danley@gmail.com>
2014-03-30 03:08:53 -0400

    Fixed DicttoJS and DataDict so that things aren't so bad.

--- scripts/data/DicttoJS.py
+++ scripts/data/DicttoJS.py
@@ -2,7 +2,7 @@ f = open("DataDict.txt")
 lines = f.readlines()
 
 #Format into data
-data = [i.strip() for i in lines]
+data = [i.strip().split() for i in lines]
 
 #From original... ancient code 
 #	  _A_
@@ -10,12 +10,17 @@ data = [i.strip() for i in lines]
 #  |__/*\
 #  | /   \
 #
-# AnCiEnT WiZaRd!
+# AnCiEnT WiZ0rd!
 
-data = [[i[:10].strip()] + [i[10:115].strip()] + i[115:].strip().split() for i in data]
+print(data[0])
+data[1] = [data[1][0]] + [" ".join(data[1][1:])]
+for i in range(2,len(data)):
+	data[i] = [data[i][0]] + [" ".join(data[i][1:-6])] + data[i][-6:]
+
+#data = [[i[:10].strip()] + [i[10:115].strip()] + i[115:].strip().split() for i in data]
 
 #3,4,5, and 6 are numeric entrees
-data = [data[0]] + [i[:3] + map(float,i[3:7]) + i[7:] for i in data[1:]]
+data = data[0:2] + [i[:3] + map(float,i[3:7]) + i[7:] for i in data[2:]]
 
 f = open("DataDict.js", "w")
 f.write("var DataDict = ")

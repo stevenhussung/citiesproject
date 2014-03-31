d5a38c7c3b6f655e7a8f2fe1eec1cc91064c6043
BeaumontTaz <bryan.b.danley@gmail.com>
2014-03-30 22:58:52 -0400

    Added return to search feature with all desired functionality. Still have error with long county names bleeding to next line.

--- scripts/cities.js
+++ scripts/cities.js
@@ -1,10 +1,15 @@
 var matches = new Array(); //To be populated with a list of matches
+var s; // Search criteria
+var sort_type; // sort type
+var scroll_pos; // scroll position
 
 function display_county(i)
 {
 	// i is the index in FIPS_CountyName
 	// j will be the index in DataSet
 	// DataSet has 53 more elements than FIPS_CountyName does
+	scroll_pos = $("html").scrollTop();
+	sort_type = $("#sort_by").val();
 	var j = i; //(guess here)i
 	var j2 = j+53;
 	var jtmp;
@@ -22,16 +27,30 @@ function display_county(i)
 	}
 	var F = FIPS_CountyName[i];
 	var D = DataSet[j];
-	$("#content").html("<h3>"+F[1]+" "+F[3]+", "+F[2]+"</h3><br><table id='county_data'></table>");
+	$("#content").html("<h3>"+F[1]+" "+F[3]+", "+F[2]+"</h3><br><table id='county_data'></table> <div onclick='return_search()'>Return to Search</div>");
 	for (k = 1; k < D.length; k++)
 	{	
 		$("#county_data").append("<tr><td>"+DataDict[k+1][1]+"</td><td>"+D[k]+"</td></tr>");
 	}
+	$("html").scrollTop(0);
 }
 
+function return_search()
+{
+	$("#content").html("Cities of Macon lets users cities similar to each other using U.S. Census Data. Our end goal is to design and implement a smooth user experiment in searching, displaying, and analyzing results. (PATRICK WRITE THIS!)<div id='search_div'><center><input type='text' name='search' id='search' value='Search by County' onfocus='if(this.value==this.defaultValue){this.value=\"\"}'><button onclick='search_county()'>Search</button>	or <select name='state' id='state'><option value = 'default'>Select a State</option><option value = '01'>Alabama</option><option value = '02'>Alaska</option><option value = '04'>Arizona</option><option value = '05'>Arkansas</option><option value = '06'>California</option><option value = '08'>Colorado</option><option value = '09'>Connecticut</option><option value = '10'>Delaware</option><option value = '11'>District Of Columbia</option><option value = '12'>Florida</option><option value = '13'>Georgia</option><option value = '15'>Hawaii</option><option value = '16'>Idaho</option><option value = '17'>Illinois</option><option value = '18'>Indiana</option><option value = '19'>Iowa</option><option value = '20'>Kansas</option><option value = '21'>Kentucky</option><option value = '22'>Louisiana</option><option value = '23'>Maine</option><option value = '24'>Maryland</option><option value = '25'>Massachusetts</option><option value = '26'>Michigan</option><option value = '27'>Minnesota</option><option value = '28'>Mississippi</option><option value = '29'>Missouri</option><option value = '30'>Montana</option><option value = '31'>Nebraska</option><option value = '32'>Nevada</option><option value = '33'>New Hampshire</option><option value = '34'>New Jersey</option><option value = '35'>New Mexico</option><option value = '36'>New York</option><option value = '37'>North Carolina</option><option value = '38'>North Dakota</option><option value = '39'>Ohio</option><option value = '40'>Oklahoma</option><option value = '41'>Oregon</option><option value = '42'>Pennsylvania</option><option value = '44'>Rhode Island</option><option value = '45'>South Carolina</option><option value = '46'>South Dakota</option><option value = '47'>Tennessee</option><option value = '48'>Texas</option><option value = '49'>Utah</option><option value = '50'>Vermont</option><option value = '51'>Virginia</option><option value = '53'>Washington</option><option value = '54'>West Virginia</option><option value = '55'>Wisconsin</option><option value = '56'>Wyoming</option></select><div id='output'></div>");
+	$("#output").html("Sort by <select name='sort_by' id='sort_by' onchange='sort_by_change(this.options[this.selectedIndex].value)'><option value='0'>Relevance</option><option value='1'>State</option><option value='2'>A-Z</option></select><br/>");
+	$("#output").append("<table cellspace='0' cellpadding='0'></table>");
+	$("#search").val(s);
+	print_matches()
+	$("html").scrollTop(scroll_pos);
+	$("#sort_by").val(sort_type);
+	
+}
+
+			
 function search_county()
 {
-	var s = ($("#search").val()).toLowerCase();
+	s = ($("#search").val()).toLowerCase();
 
 	// Check if search criteria has been entered.
 	if (s == "search by county")
